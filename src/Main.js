d651576d785e9b6c37aca5e026212c44ec5064a2
Steve Weil <steve.weil@smartsheet.com>
2018-04-14 23:34:40 -0700

    Almost have nav
    Following instructions from: https://www.kirupa.com/react/creating_single_page_app_react_using_react_router.htm

--- src/Main.js
+++ src/Main.js
@@ -1,33 +1,42 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import { Route, NavLink, HashRouter } from "react-router-dom";
+import NavComponent from "./NavComponent"
 import Home from "./Home";
 import Stuff from "./Stuff";
 import Contact from "./Contact";
 
 class Main extends Component {
     render() {
-        return <HashRouter>
-                <div>
-                    <h1>Simple SPA</h1>
-                    <ul className="header">
-                        <li>
-                            <NavLink to="/">Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/stuff">Stuff</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/contact">Contact</NavLink>
-                        </li>
-                    </ul>
-                    <div className="content">
-                    
-            <Route exact path="/" component={Home}/>
-            <Route path="/stuff" component={Stuff}/>
-            <Route path="/contact" component={Contact}/>
-         </div>
-                </div>
-            </HashRouter>;
+        return (
+            <div>
+                Hi
+                <navbar>
+                    {ReactDOM.render(<NavComponent />, document.querySelector('navbar'))}
+                    </navbar>
+                <HashRouter>
+                    <div>
+                        <h1>Simple SPA</h1>
+                        <ul className="header">
+                            <li>
+                                <NavLink to="/">Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/stuff">Stuff</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/contact">Contact</NavLink>
+                            </li>
+                        </ul>
+                        <div className="content">
+                            <Route exact path="/" component={Home} />
+                            <Route path="/stuff" component={Stuff} />
+                            <Route path="/contact" component={Contact} />
+                        </div>
+                    </div>
+                </HashRouter>
+            </div>
+        );
     }
 }
 
