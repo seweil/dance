3d15e2e34288e7d4cafdf3dcd424af5463dee831
Steve Weil <steve@theweils.com>
2018-05-26 22:04:20 -0700

    Edit on gh.com

--- patch.bash
+++ patch.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
-echo Updating date
+echo Updating date2
 SRC="src/Home.js"
 TEMP_FILE="_tmp"
 sed "s-<p>Updated: .*</p>-<p>Updated: `date`</p>-" $SRC > $TEMP_FILE
 mv -f $TEMP_FILE $SRC
-
