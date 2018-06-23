fac98434d9a95d0c000ef5e356ce9347bd0e3943
Steve Weil <steve.weil@smartsheet.com>
2018-06-23 15:56:15 -0700

    Sched first light

--- src/danceSchedule.js
+++ src/danceSchedule.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import Image from "./Image";
 import ReactGA from 'react-ga';
 
+import './generatedStyles.css';
+import htmlSchedule from "./html/Schedule.html";
+
 
 import danceSchedule from "./assets/schedule.png"
 class DanceSchedule extends Component {
@@ -12,6 +15,7 @@ class DanceSchedule extends Component {
         return (
              <div>
                 <h2>Dance Schedule</h2>
+                <div dangerouslySetInnerHTML= { {__html: htmlSchedule }} />>
                 <Image src={danceSchedule} mode='fit'/>
 
             </div>
