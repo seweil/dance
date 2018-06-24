a961e5d1c25699a04c48ffd994f37dc6596681d3
Steve Weil <steve.weil@smartsheet.com>
2018-06-24 12:59:34 -0700

    Persiste!

--- src/danceSchedule.js
+++ src/danceSchedule.js
@@ -5,8 +5,8 @@ import 'rc-slider/assets/index.css';
 import './generatedStyles.css';
 import htmlSchedule from "./html/Schedule.html";
 
-var modifyClassNames = (function(selectorList, className, add) {
-    if (! Array.isArray(selectorList)) {
+var modifyClassNames = (function (selectorList, className, add) {
+    if (!Array.isArray(selectorList)) {
         selectorList = [selectorList];
     }
 
@@ -23,8 +23,8 @@ var modifyClassNames = (function(selectorList, className, add) {
     }
 });
 
-var modifyAttribute = (function(selectorList, attribute, value) {
-    if (! Array.isArray(selectorList)) {
+var modifyAttribute = (function (selectorList, attribute, value) {
+    if (!Array.isArray(selectorList)) {
         selectorList = [selectorList];
     }
 
@@ -38,22 +38,70 @@ var modifyAttribute = (function(selectorList, attribute, value) {
 });
 
 class DanceSchedule extends Component {
+    rangeKey = "range";
+    gcaKey = "showGca"
+    specialtyKey = "showSpecialty";
 
+    constructor(props) {
+        super(props);
+
+         // Defaults
+         var min = 1;
+         var max = 7;
+         var showGca = true;
+         var showSpecialty = true;
+ 
+         // Read saved values
+         if (localStorage) {
+             var savedRangeString = localStorage.getItem(this.rangeKey);
+             if (savedRangeString) {
+                 var savedRange = JSON.parse(savedRangeString);
+                 if (savedRange && savedRange.min && savedRange.max) {
+                     min = savedRange.min;
+                     max = savedRange.max;
+                 }
+             }
+ 
+             var savedShowGcaString = localStorage.getItem(this.gcaKey);
+             if (savedShowGcaString) {
+                 var savedShowGca = JSON.parse(savedShowGcaString);
+                 showGca = !!savedShowGca;
+             }
+
+             var savedShowSpecialtyString = localStorage.getItem(this.specialtyKey);
+             if (savedShowSpecialtyString) {
+                 var savedShowSpecialty = JSON.parse(savedShowSpecialtyString);
+                 showSpecialty = !!savedShowSpecialty;
+             }
+         }
+        this.state = {
+            showGca,
+            showSpecialty,
+            min,
+            max
+        }
+    }
 
     gcaChanged(event) {
-        const target = event.target;
-        const value = !target.checked;
-        console.log("Changed", event, value);
+        const show = event.target.checked;
+        console.log("Changed GCA", show);
+        this.updateGca(show);
+    }
 
-        modifyClassNames(["GCA", "GCA-squeeze"], "hidden", value);
+    updateGca(show) {
+        modifyClassNames(["GCA", "GCA-squeeze"], "hidden", !show);
+        localStorage.setItem(this.gcaKey, JSON.stringify(show));
     }
 
     specialtyChanged(event) {
-        const target = event.target;
-        const value = !target.checked;
-        console.log("Changed", event, value);
+        const show = event.target.checked;
+        console.log("Changed specialty", show);
+        this.updateSpecialty(show);
+    }
 
-        modifyClassNames(["Specialty", "hideWithSpecialty"], "hidden", value);
+    updateSpecialty(show) {
+        modifyClassNames(["Specialty", "hideWithSpecialty"], "hidden", !show);
+        localStorage.setItem(this.specialtyKey, JSON.stringify(show));
     }
 
     rangeChanged(values) {
@@ -61,51 +109,61 @@ class DanceSchedule extends Component {
         var min = values[0];
         var max = values[1];
 
-        modifyClassNames(["MS", "hideWithMS"], "hidden", min > 0);
-        modifyClassNames(["Plus", "hideWithPlus"], "hidden", min > 1 || max < 1);
-        modifyClassNames(["Adv", "hideWithAdv"], "hidden", min > 2 || max < 2);
-        modifyClassNames(["C1", "hideWithC1"], "hidden", min > 3 || max < 3);
-        modifyClassNames(["C2", "hideWithC2"], "hidden", min > 4 || max < 4);
-        modifyClassNames(["C3a", "hideWithC3a"], "hidden", min > 5 || max < 5);
-        modifyClassNames(["C3b", "hideWithC3b", "C4"], "hidden", min > 6 || max < 6);
+        modifyClassNames(["MS", "hideWithMS"], "hidden", min > 1);
+        modifyClassNames(["Plus", "hideWithPlus"], "hidden", min > 2 || max < 2);
+        modifyClassNames(["Adv", "hideWithAdv"], "hidden", min > 3 || max < 3);
+        modifyClassNames(["C1", "hideWithC1"], "hidden", min > 4 || max < 4);
+        modifyClassNames(["C2", "hideWithC2"], "hidden", min > 5 || max < 5);
+        modifyClassNames(["C3a", "hideWithC3a"], "hidden", min > 6 || max < 6);
+        modifyClassNames(["C3b", "hideWithC3b", "C4"], "hidden", min > 7 || max < 7);
 
         var spanCount = max - min + 1;
         modifyAttribute("Merged", "colspan", spanCount);
+
+        localStorage.setItem(this.rangeKey, JSON.stringify({ min, max }));
+    }
+
+
+    componentDidMount() {
+        // Update display
+        this.rangeChanged([this.state.min, this.state.max]);
+        this.updateGca(this.state.showGca);
+        this.updateSpecialty(this.state.showSpecialty);
     }
 
     render() {
-        ReactGA.set({ "page": "Schedule"});        
+        ReactGA.set({ "page": "Schedule" });
         ReactGA.pageview("/Schedule");
 
-        var marks = { 0: "MS", 1: "Plus", 2: "Adv", 3: "C1", 4: "C2", 5: "C3a", 6: "C3b+" };
+        var marks = { 1: "MS", 2: "Plus", 3: "Adv", 4: "C1", 5: "C2", 6: "C3a", 7: "C3b+" };
         return (
             <div>
-             <div className="CalendarControls">
-                <h2>Dance Schedule</h2>
+                <div className="CalendarControls">
+                    <h2>Dance Schedule</h2>
+                    <span>
+                        <input type="checkbox" id="showGca" onChange={this.gcaChanged.bind(this)} defaultChecked={this.state.showGca} />
+                        <label htmlFor="showGca">Show GCA</label>
+                    </span>
+                    &nbsp;&nbsp;&nbsp;
                 <span>
-                    <input type="checkbox" id="showGca" onChange= {this.gcaChanged} defaultChecked={ true }/>
-                    <label htmlFor="showGca">Show GCA</label>
-                </span>
-                &nbsp;&nbsp;&nbsp;
-                <span>
-                    <input type="checkbox" id="showSpecialty" onChange= {this.specialtyChanged} defaultChecked={ true }/>
-                    <label htmlFor="showSpecialty">Show Specialty</label>
-                </span>
-                <br/>
-                <div className="rangeContainer">
-                    <Range
-                        allowCross={ false}
-                        min={ 0 }
-                        max={ 6 }
-                        marks={ marks }
-                        defaultValue= { [0, 6] }
-                        onChange={ this.rangeChanged }
-                    />
+                        <input type="checkbox" id="showSpecialty" onChange={this.specialtyChanged.bind(this)} defaultChecked={this.state.showSpecialty} />
+                        <label htmlFor="showSpecialty">Show Specialty</label>
+                    </span>
+                    <br />
+                    <div className="rangeContainer">
+                        <Range
+                            allowCross={false}
+                            min={1}
+                            max={7}
+                            marks={marks}
+                            defaultValue={[this.state.min, this.state.max]}
+                            onChange={this.rangeChanged}
+                        />
+                    </div>
+                    <br /><br />
                 </div>
-                <br/><br/>
-</div>
 
-                <div dangerouslySetInnerHTML= { {__html: htmlSchedule }} />>
+                <div dangerouslySetInnerHTML={{ __html: htmlSchedule }} />>
 
             </div>
         );
