68f5935e788b874db4748e9d75d936fd0c19f407
Steve Weil <steve.weil@smartsheet.com>
2018-06-23 19:34:09 -0700

    Toggle GCA

--- src/danceSchedule.js
+++ src/danceSchedule.js
@@ -1,13 +1,43 @@
 import React, { Component } from "react";
-import Image from "./Image";
 import ReactGA from 'react-ga';
 
 import './generatedStyles.css';
 import htmlSchedule from "./html/Schedule.html";
 
+var modifyClassNames = (function(selector, className, add) {
+    var items = document.getElementsByClassName(selector);
+    for (var i = 0; i < items.length; i++ ){
+        if (add) {
+            items[i].classList.add(className);
+        } else {
+            items[i].classList.remove(className);
+        }
+    }
+});
 
-import danceSchedule from "./assets/schedule.png"
 class DanceSchedule extends Component {
+
+
+    gcaChanged(event) {
+        const target = event.target;
+        const value = !target.checked;
+        console.log("Changed", event, value);
+
+        modifyClassNames("GCA", "hidden", value);
+        modifyClassNames("GCA-squeeze", "hidden", value);
+/*
+        var selects = document.getElementsByClassName("GCA");
+        for(var i =0, il = selects.length;i<il;i++){
+           selects[i].className += " hidden";
+        }
+
+        var selects = document.getElementsByClassName("GCA-squeeze");
+        for(var i =0, il = selects.length;i<il;i++){
+           selects[i].className += " hidden";
+        }
+        */
+    }
+
     render() {
         ReactGA.set({ "page": "Schedule"});        
         ReactGA.pageview("/Schedule");
@@ -15,11 +45,17 @@ class DanceSchedule extends Component {
         return (
              <div>
                 <h2>Dance Schedule</h2>
+                <div>
+                    <input type="checkbox" id="showGca" onChange= {this.gcaChanged} defaultChecked={ true }/>
+                    <label htmlFor="showGca">Show GCA</label>
+                </div>
+
                 <div dangerouslySetInnerHTML= { {__html: htmlSchedule }} />>
-                <Image src={danceSchedule} mode='fit'/>
 
             </div>
         );
     }
+
+
 }
 export default DanceSchedule;
